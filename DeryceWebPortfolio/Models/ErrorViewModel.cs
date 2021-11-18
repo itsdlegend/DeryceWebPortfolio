33f58e8935d7fa5a71cc851b66ab328f40ee3334
Deryce Messam <dmessam@outlook.com>
2021-11-18 16:48:58 +0000

    Add project files.

---
+++ DeryceWebPortfolio/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DeryceWebPortfolio.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
